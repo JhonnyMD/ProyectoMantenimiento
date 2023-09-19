efdb2faef54793ec625c7e35de80dd03adf3c611
JhonnyMD <jhonnyrx90@gmail.com>
2023-09-19 13:10:01 -0500

    PRIMER MODELO

---
+++ src/co/edu/unicolombo/pb/ProyectoMantenimiento.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package co.edu.unicolombo.pb;
+
+import co.edu.unicolombo.pb.ventanas.VentanaPrincipal;
+
+/**
+ *
+ * @author POWERGROUP
+ */
+public class ProyectoMantenimiento {
+
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal ventana = new VentanaPrincipal();
+         ventana.setLocationRelativeTo(null);
+       // ventana.setExtendedState(VentanaPrincipal.MAXIMIZED_BOTH);
+        ventana.setVisible(true);
+        
+        
+        
+    }
+    
+}
---
+++ src/co/edu/unicolombo/pb/datos/Salones.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.unicolombo.pb.datos;
+
+import java.util.*;
+
+
+/**
+ *
+ * @author POWERGROUP
+ */
+public class Salones {
+    public String nombresalon;
+    public String dispositivo;
+    public String dispositivo2;
+    public String dispositivo3;
+    public ArrayList<Dispositivos> listadedispositivos;
+    
+    
+}
