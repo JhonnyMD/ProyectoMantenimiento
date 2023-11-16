c1e43d4e4186815db6c909802095c6c9723b92f6
JhonnyMD <jhonnyrx90@gmail.com>
2023-11-15 21:23:39 -0500

    Merge origin/master
    Conflicts:
    	nbproject/genfiles.properties
    	src/co/edu/unicolombo/pb/ProyectoMantenimiento.java
    
    INTERFAZ GRAFICA MEJORADA (HECHO POR DULCE)

---
+++ src/co/edu/unicolombo/pb/ProyectoMantenimiento.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package co.edu.unicolombo.pb;
+
+import co.edu.unicolombo.pb.ventanas.VentanaPrincipal2;
+
+/**
+ *
+ * @author POWERGROUP
+ */
+public class ProyectoMantenimiento {
+
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal2 ventana = new VentanaPrincipal2();
+         ventana.setLocationRelativeTo(null);
+       // ventana.setExtendedState(VentanaPrincipal.MAXIMIZED_BOTH); comentario de pantalla completa..
+        ventana.setVisible(true);
+        
+    
+    }
+    
+}
